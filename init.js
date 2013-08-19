28f2831b3b7eab9c6211bc46f63d20e762fb66dd
Jeroen Oliemans <jeroenoliemans@gmail.com>
2013-08-19 14:26:19 +0200

    added normalize.css

--- init.js
+++ init.js
@@ -1,8 +1,7 @@
 /* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * @param: elGallery, selector of the containing UL
+ * 
  */
-
 var plainGallery = PlainGallery('#galleryList');
 
 
